cf72bfa2e5b2a0dd88f32543c89cd8ffd5d121ca
lakshmik98 <lakshmikonda.neo@gmail.com>
2022-02-09 12:00:00 +0530

    first commit from lakshman

---
+++ class_sub_project.java
@@ -0,0 +1,8 @@
+class lakshman
+{
+public static void main(String args[])
+{
+System.out.println("Welcome to the lakshman project");
+System.out.println("Egarly waiting for the next challenges");
+}
+}
